3716c479aa2c30f768c355747acdc3e044d6e411
mansonwuhoman <mansonwuhoman@gmail.com>
2020-08-06 19:27:46 +0800

    first commit

---
+++ src/test/kotlin/org/mwlab/fullstack/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.mwlab.fullstack.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
